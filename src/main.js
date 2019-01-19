d0c1403cfaa4b7f1ef04394df26b0dbacc5db125
George Jempty <consult@dexygen.com>
2019-01-19 13:07:18 +0000

    Comparison between wasm and javascript consistently shows a 40% gain

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+function fibonacci(sequenceFor) {
+  if (sequenceFor <= 1) return 1;
+  return fibonacci(sequenceFor - 1) + fibonacci(sequenceFor - 2);
+}
+
 WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
   env: {
     abort(msg, file, line, column) {
@@ -6,8 +11,26 @@ WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
   },
 }).then(result => {
   const exports = result.instance.exports;
-  const beganAt = new Date().getTime();
-  const fib45 = exports.fibonacci(45);
-  console.log(new Date().getTime() - beganAt);
-  console.log(fib45);
+  let beganAt;
+  let fib45;
+  let totalAsElapsed = 0;
+  let totalJsElapsed = 0;
+  
+  for (let i = 1, n = 500; i<=n; i++) {
+	  beganAt = new Date().getTime();
+	  fib45 = exports.fibonacci(30);
+	  totalAsElapsed += ((new Date().getTime() - beganAt) / 1000);
+	  
+	  beganAt = new Date().getTime();
+	  fib45 = fibonacci(30);
+	  totalJsElapsed += ((new Date().getTime() - beganAt) / 1000);
+	  
+	  if (!(i%50)) console.log(i, 'of ', n, 'iterations....');
+  }
+  
+  console.log('totalAsElapsed', totalAsElapsed);
+  console.log('totalJsElapsed', totalJsElapsed);
+  let asElapsedGainPct = ((totalJsElapsed/totalAsElapsed) - 1) * 100;
+  console.log('asElapsedGain (%)', asElapsedGainPct);
+  
 }).catch(console.error);
